976b9fbf7b11915aea8fe29164b5cf345c2a1182
liam-rng <liam.scott.7798@gmail.com>
2022-06-23 15:08:39 -0400

    Format code, set a fixed size for the container, add code for size btn
    The choose size button takes a value and changes the size of the sketch-
    pad, however, after the change the dots no longer change color.

--- scripts.js
+++ scripts.js
@@ -1,11 +1,18 @@
-var side = window.prompt(`How many squares should the square have per side?`);
 const container = document.querySelector(`#container`)
+const choseSize = document.querySelector(`#size_changer`);
 
-const n = side * side;
-document.getElementById(`container`).style.gridTemplateColumns = `repeat(${side},1fr)`
-for(let i = 0; i < n; i++){
-    container.innerHTML += `<div class="dot"></div>`;
-}
+const createContainer = function (side, total) {
+    document.getElementById(`container`).style.gridTemplateColumns = `repeat(${side},1fr)`
+    container.innerHTML = ``;
+    for (let i = 0; i < total; i++) {
+        container.innerHTML += `<div class="dot"></div>`;
+    };
+};
+
+
+let side = 8;
+const total = side * side;
+createContainer(side, total);
 
 const dots = document.querySelectorAll(`.dot`);
 
@@ -13,16 +20,22 @@ buttons = document.querySelectorAll(`button`);
 let styling_str = `background-color:`;
 let color;
 buttons.forEach(button => {
-    button.addEventListener(`click`, function buttonClick(event){
-        if(event.target.id !== `size_changer`){
-        color = event.target.id;
-         console.log(color);}
+    button.addEventListener(`click`, function buttonClick(event) {
+        if (event.target.id == `size_changer`) {
+            user_side = window.prompt(`How many squares should the square have per side?`);
+            user_total = user_side * user_side;
+            createContainer(user_side, user_total);
+        }
+        else {
+            color = event.target.id;
+        }
     });
 });
 
 dots.forEach(dot => {
-    dot.addEventListener('mouseover', function handleHover(event) {
-      let selected_color = styling_str + color + `;`;
+    dot.addEventListener('mouseover', function handleHover() {
+        let selected_color = styling_str + color + `;`;
         dot.setAttribute('style', selected_color);
     });
 });
+
