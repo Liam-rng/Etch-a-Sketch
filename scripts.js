0fdbaf255fa66b69c2030a7dac8e7b6fae573ad3
liam-rng <liam.scott.7798@gmail.com>
2022-06-23 13:03:50 -0400

    Add color buttons, script to change colors, and minor style changes
    Changes:
    index.html
    Added color buttons
    
    scripts.js
    Added event listener to the buttons so that a new background
    color is added to the div when hovered over
    
    styles.css
    Added style to buttons, and a border to the container.

--- scripts.js
+++ scripts.js
@@ -1,19 +1,28 @@
-
+var side = window.prompt(`How many squares should the square have per side?`);
 const container = document.querySelector(`#container`)
 
-const n = 256;
-
+const n = side * side;
+document.getElementById(`container`).style.gridTemplateColumns = `repeat(${side},1fr)`
 for(let i = 0; i < n; i++){
     container.innerHTML += `<div class="dot"></div>`;
 }
 
 const dots = document.querySelectorAll(`.dot`);
 
-let selected_color = `background-color: red;`;
 
+buttons = document.querySelectorAll(`button`);
+let styling_str = `background-color:`;
+let color;
+buttons.forEach(button => {
+    button.addEventListener(`click`, function colorSelection(event){
+        color = event.target.id;
+         console.log(color);
+    });
+});
 
 dots.forEach(dot => {
     dot.addEventListener('mouseover', function handleClick(event) {
-      dot.setAttribute('style', selected_color);
+      let selected_color = styling_str + color + `;`;
+        dot.setAttribute('style', selected_color);
     });
 });
